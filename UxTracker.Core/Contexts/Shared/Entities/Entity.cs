0f592f6d2c0dfb9bed2e7d54519ed38dd75cb93b
rhogger <rhoggerrv@gmail.com>
2024-06-26 19:37:30 -0300

    :label: Criado super classes para herança de entities e value objects

---
+++ UxTracker.Core/Contexts/Shared/Entities/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace UxTracker.Core.Contexts.Shared.Entities;
+
+public abstract class Entity : IEquatable<Guid>
+{
+    protected Entity() => Id = Guid.NewGuid();
+
+    public Guid Id { get; }
+
+    public bool Equals(Guid id) => Id == id;
+
+    public override int GetHashCode() => Id.GetHashCode();
+}
---
+++ UxTracker.Core/Contexts/Shared/ValueObjects/ValueObject.cs
@@ -0,0 +1,3 @@
+namespace UxTracker.Core.Contexts.Shared.ValueObjects;
+
+public abstract class ValueObject { }
