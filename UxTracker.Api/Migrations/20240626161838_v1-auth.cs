7e3a16f44cc2b46aa708f2e746f445f1724fd3d0
rhogger <rhoggerrv@gmail.com>
2024-06-26 19:44:54 -0300

    :card_file_box: raw: Feito as migrations para geração das tabelas no banco

---
+++ UxTracker.Api/Migrations/20240626161838_v1-auth.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UxTracker.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1auth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR(80)", maxLength: 80, nullable: false),
+                    Email = table.Column<string>(type: "VARCHAR(255)", maxLength: 255, nullable: false),
+                    EmailVerificationCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailVerificationExpireAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EmailVerificationVerifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordResetCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "VARCHAR(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ UxTracker.Api/Migrations/20240626163915_v1.1-auth.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UxTracker.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class v11auth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordResetCode",
+                table: "Users",
+                type: "NVARCHAR(8)",
+                maxLength: 8,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "NVARCHAR(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailVerificationCode",
+                table: "Users",
+                type: "NVARCHAR(6)",
+                maxLength: 6,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordResetCode",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(8)",
+                oldMaxLength: 8);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailVerificationCode",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(6)",
+                oldMaxLength: 6);
+        }
+    }
+}
