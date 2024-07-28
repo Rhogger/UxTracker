9ae3a9197f3c80a89700f3e39b49df51f60e61f4
rhogger <rhoggerrv@gmail.com>
2024-07-28 20:56:58 -0300

    :sparkles: feat: Adicionado validação do formulário e feito a injeção de dependencia no Verify

--- UxTracker.Researchers.Web/Pages/Contexts/Account/UseCases/Verify/AccountVerificationPage.razor.cs
+++ UxTracker.Researchers.Web/Pages/Contexts/Account/UseCases/Verify/AccountVerificationPage.razor.cs
@@ -1,28 +1,97 @@
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 using RestSharp;
 using UxTracker.Core;
+using UxTracker.Core.Contexts.Account.Validations;
 
 namespace UxTracker.Researchers.Web.Pages.Contexts.Account.UseCases.Verify;
 
 public partial class AccountVerification: ComponentBase
 {
+    [Inject] protected NavigationManager Navigation { get; set; } 
+    [Inject] protected ISnackbar Snackbar { get; set; }
+    [Inject] protected IRestClient RestClient { get; set; }
+    
+    //TODO: Alterar essa passagem de parametros como o email que está nos cookies
     protected Core.Contexts.Account.UseCases.Verify.Request Req = new();
+    
+    protected MudForm Form;
+    protected string[] Errors = Array.Empty<string>();
+    protected bool IsValid;
 
     protected async Task VerifyAsync()
     {
-        var client = new RestClient();
-        var request = new RestRequest("/api/v1/verify")
+        var request = new RestRequest("/api/v1/verify", Method.Patch)
             .AddJsonBody(Req);
-        await client.PatchAsync<Core.Contexts.Account.UseCases.Verify.Request>(request);
+        try
+        {
+            var response = await RestClient.ExecuteAsync<Core.Contexts.Account.UseCases.Verify.Response>(request);
+
+            if (response.Data is not null)
+            {
+                if (response.IsSuccessful)
+                {
+                    if (response.Data.StatusCode == 200)
+                    {
+                        Snackbar.Add(response.Data.Message, Severity.Success);
+                        Navigation.NavigateTo("/");
+                    }
+                    else
+                        Snackbar.Add($"Erro: {response.Data.StatusCode} - {response.Data.Message}", Severity.Error);
+                
+                }
+                else
+                {
+                    if (response.Data.Notifications is not null)
+                        foreach (var notification in response.Data.Notifications)
+                            Snackbar.Add(notification.Message, Severity.Error);
+                    else
+                        Snackbar.Add($"Erro: {response.Data.StatusCode} - {response.Data.Message}", Severity.Error);
+                }
+            }
+            else
+                Snackbar.Add($"Erro: {response.StatusCode} - {response.Content}", Severity.Error);
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add($"Exception: {ex.Message}", Severity.Error);
+        }
     }
 
     protected async Task ResendVerificationCodeAsync()
     {
+        //TODO: Alterar essa passagem de parametros como o email que está nos cookies
+        //TODO: Corrigir validação, porque essa requisição só pode ser feita se o campo do codigo de verificação for validado
         Core.Contexts.Account.UseCases.ResendVerificationCode.Request req = new(Req.Email);
         
-        var client = new RestClient();
-        var request = new RestRequest("/api/v1/resend-verification-code")
+        var request = new RestRequest("/api/v1/resend-verification-code", Method.Patch)
             .AddJsonBody(req);
-        await client.PatchAsync<Core.Contexts.Account.UseCases.ResendVerificationCode.Request>(request);
+        
+        try
+        {
+            var response = await RestClient.ExecuteAsync<Core.Contexts.Account.UseCases.ResendVerificationCode.Response>(request);
+
+            if (response is { IsSuccessful: true, Data: not null })
+            {
+                if (response.Data.StatusCode == 200)
+                    Snackbar.Add(response.Data.Message, Severity.Success);
+                else
+                {
+                    if (response.Data.Notifications is not null)
+                        foreach (var notification in response.Data.Notifications)
+                            Snackbar.Add(notification.Message, Severity.Error);
+                    else
+                        Snackbar.Add($"Erro: {response.Data.StatusCode} - {response.Data.Message}", Severity.Error);
+                }
+            }
+            else
+                Snackbar.Add($"Erro: {response.StatusCode} - {response.Content}", Severity.Error);
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add($"Exception: {ex.Message}", Severity.Error);
+        }
     }
+    
+
 }
