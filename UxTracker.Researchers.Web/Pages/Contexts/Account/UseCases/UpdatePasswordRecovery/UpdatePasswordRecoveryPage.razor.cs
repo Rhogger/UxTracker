405d044e95020988156659f91eefda5c8b9cc7ec
rhogger <rhoggerrv@gmail.com>
2024-08-24 15:53:40 -0300

    :recycle: refactor: Refatorando a página do Authenticate, Create, PasswordRecovery, UpdatePasswordAuthenticated, UpdatePasswordRecovery e Verify

--- UxTracker.Researchers.Web/Pages/Contexts/Account/UseCases/UpdatePasswordRecovery/UpdatePasswordRecoveryPage.razor.cs
+++ UxTracker.Researchers.Web/Pages/Contexts/Account/UseCases/UpdatePasswordRecovery/UpdatePasswordRecoveryPage.razor.cs
@@ -1,84 +1,50 @@
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
-using RestSharp;
-using UxTracker.Core.Contexts.Account.UseCases.UpdatePassword;
+using UxTracker.Core.Contexts.Account.Handlers;
+using UpdatePasswordUseCase = UxTracker.Core.Contexts.Account.UseCases.UpdatePassword;
 
 namespace UxTracker.Researchers.Web.Pages.Contexts.Account.UseCases.UpdatePasswordRecovery;
 
 public partial class UpdatePasswordRecovery : ComponentBase
 {
-    [Inject] protected NavigationManager Navigation { get; set; } 
-    [Inject] protected ILocalStorageService LocalStorage { get; set; }
-    [Inject] protected ISnackbar Snackbar { get; set; }
-    [Inject] protected IRestClient RestClient { get; set; }
+    [Inject] protected IAccountContextHandler AccountContextHandler { get; set; } = null!;
+    [Inject] protected NavigationManager Navigation { get; set; } = null!;
+    [Inject] protected ILocalStorageService LocalStorage { get; set; } = null!;
+    [Inject] protected ISnackbar Snackbar { get; set; } = null!;
     
-    protected Request Req = new();
-    
-    protected MudForm Form;
-    protected string[] Errors = Array.Empty<string>();
-    protected bool IsValid;
-
-    protected InputType PasswordInput = InputType.Password;
-    protected string PasswordInputIcon = Icons.Material.Filled.VisibilityOff;
-    protected bool IsPasswordShow;
+    protected UpdatePasswordUseCase.Request Request = new();
     
     protected override async  Task OnInitializedAsync()
     {
-        Req.Email = await LocalStorage.GetItemAsync<string>("email") ?? string.Empty;
+        Request.Email = await LocalStorage.GetItemAsync<string>("email") ?? string.Empty;
     }
+    
     protected async Task UpdatePasswordAsync()
     {
-        var request = new RestRequest("/api/v1/password-recover/update-password", Method.Patch)
-            .AddJsonBody(Req);
         try
         {
-            var response = await RestClient.ExecuteAsync<Response>(request);
+            var response = await AccountContextHandler.UpdatePasswordAsync(Request);
 
-            if (response.Data is not null)
-            {
-                if (response.IsSuccessful)
-                {
-                    if (response.Data.StatusCode == 200)
-                    {
-                        Snackbar.Add(response.Data.Message, Severity.Success);
-                        Navigation.NavigateTo("/login");
-                    }
-                    else
-                        Snackbar.Add($"Erro: {response.Data.StatusCode} - {response.Data.Message}", Severity.Error);
-                
+            if (response is not null)
+                if (response.IsSuccessful){
+                    Snackbar.Add(response.Data!.Message, Severity.Success);
+                    Navigation.NavigateTo("/login");
                 }
                 else
                 {
-                    if (response.Data.Notifications is not null)
+                    if (response.Data!.Notifications is not null)
                         foreach (var notification in response.Data.Notifications)
                             Snackbar.Add(notification.Message, Severity.Error);
                     else
                         Snackbar.Add($"Erro: {response.Data.StatusCode} - {response.Data.Message}", Severity.Error);
                 }
-            }
             else
-                Snackbar.Add($"Erro: {response.StatusCode} - {response.Content}", Severity.Error);
+                Snackbar.Add($"Ocorreu algum erro no nosso servidor. Por favor, tente mais tarde.", Severity.Error);
         }
         catch (Exception ex)
         {
             Snackbar.Add($"Exception: {ex.Message}", Severity.Error);
-        }
-    }
-
-    public void ShowPassword()
-    {
-        if(IsPasswordShow)
-        {
-            IsPasswordShow = false;
-            PasswordInputIcon = Icons.Material.Filled.VisibilityOff;
-            PasswordInput = InputType.Password;
-        }
-        else
-        {
-            IsPasswordShow = true;
-            PasswordInputIcon = Icons.Material.Filled.Visibility;
-            PasswordInput = InputType.Text;
         }
     }
 }
