eb602df4c5252d9d0510a2643d06a5dc034218d7
rhogger <rhoggerrv@gmail.com>
2024-08-24 15:48:56 -0300

    :sparkles: feat: Adicionando os serviços do GetUser e UpdateAccount

---
+++ UxTracker.Infra/Contexts/Account/UseCases/GetUser/Repository.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using UxTracker.Core.Contexts.Account.Entities;
+using UxTracker.Core.Contexts.Account.UseCases.GetUser.Contracts;
+using UxTracker.Infra.Data;
+
+namespace UxTracker.Infra.Contexts.Account.UseCases.GetUser;
+
+public class Repository: IRepository
+{
+    private readonly AppDbContext _context;
+
+    public Repository(AppDbContext context) => _context = context;
+    
+    public async Task<User?> GetUserByIdAsync(string id, CancellationToken cancellationToken) 
+        => await _context
+            .Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id.ToString() == id, cancellationToken);
+}
