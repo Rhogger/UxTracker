fce269cf202436564a96963f1273c6c521638a40
Rhuan Pablo <86031472+RhuanRP@users.noreply.github.com>
2024-09-07 17:23:04 -0300

    Criação de páginas: 403, 404, landing page, home-researchers.

--- UxTracker.Researchers.Web/Utils/Theme/CustomTheme.cs
+++ UxTracker.Researchers.Web/Utils/Theme/CustomTheme.cs
@@ -11,14 +11,14 @@ public class CustomTheme
             Primary = Colors.Teal.Lighten1,
             Secondary = "#161B22",
             Tertiary = "#313945",
-            
+
             Background = "#0D1117",
             AppbarBackground = "#0D1117",
             Surface = "#161B22",
-            
+
             TextSecondary = "rgba(255,255,255, 0.60)",
             AppbarText = "#FFF",
-            
+
             Success = "#2EA043",
             Info = "rgba(41,178,255, 0.60)",
             InfoDarken = "#115277",
@@ -27,8 +27,8 @@ public class CustomTheme
         {
             Default = new Default()
             {
-                FontFamily = ["Raleway","sans-serif"],
-                
+                FontFamily = ["Inter", "Roboto", "sans-serif"],
+
             },
             Button = new Button()
             {
