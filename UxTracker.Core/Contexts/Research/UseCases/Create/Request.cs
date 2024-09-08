d0be53832bd7c0960edbb61cb99de82955956970
rhogger <rhoggerrv@gmail.com>
2024-09-08 13:08:21 -0300

    Atualizando

--- UxTracker.Core/Contexts/Research/UseCases/Create/Request.cs
+++ UxTracker.Core/Contexts/Research/UseCases/Create/Request.cs
@@ -1,5 +1,5 @@
+using System.Text.Json.Serialization;
 using MediatR;
-using UxTracker.Core.Contexts.Research.Entities;
 using UxTracker.Core.Contexts.Research.Enums;
 
 namespace UxTracker.Core.Contexts.Research.UseCases.Create;
@@ -12,7 +12,6 @@ public class Request : IRequest<Response>
     public string Description { get; set; } = string.Empty;
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public int PeriodType { get; set; }
     public int Period { get; set; }
     public PeriodType PeriodType { get; set; }
     // public byte[]? ConsentTerm { get; set; } = null!;
