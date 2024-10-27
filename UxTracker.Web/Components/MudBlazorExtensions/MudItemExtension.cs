e26748c4b1d6998264a04d10952129341816fd80
rhogger <rhoggerrv@gmail.com>
2024-10-27 18:35:13 -0300

    :iphone: style: Responsividade da tela de Projeto

---
+++ UxTracker.Web/Components/MudBlazorExtensions/MudItemExtension.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using MudBlazor;
+using MudBlazor.Utilities;
+
+namespace UxTracker.Web.Components.MudBlazorExtensions;
+
+public class GridItem : MudItem
+{
+    /// <summary>
+    /// Sets the number of columns to occupy at the 'extra extra small' breakpoint.
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Item.Behavior)]
+    public int xxs { get; set; }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        base.BuildRenderTree(builder);
+    
+        var extraClasses = new CssBuilder()
+            .AddClass($"mud-grid-item-xxs-{xxs}", xxs != 0)
+            .Build();
+
+        Class = $"{Class} {extraClasses}";
+    }}
